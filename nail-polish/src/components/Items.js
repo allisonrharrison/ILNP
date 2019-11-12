c6f6f83027ff6846c83701e27a98b1a9d9e2a7c8
Allison Harrison <allisonrharrison@gmail.com>
2019-11-12 14:10:48 -0800

    Breadcrumbs & Foundational Render

--- nail-polish/src/components/Items.js
+++ nail-polish/src/components/Items.js
@@ -1,51 +1,70 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { addToCart } from './actions/cartActions'
+import React, { Component } from "react";
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { addToCart } from "./actions/cartActions";
 
- class Items extends Component{
-    
-    handleClick = (id)=>{
-        this.props.addToCart(id); 
-    }
+class Items extends Component {
+  handleClick = id => {
+    this.props.addToCart(id);
+  };
 
-    render(){
-        let itemList = this.props.items.map(item=>{
-            return(
-                <div className="card" key={item.id}>
-                        <div className="card-image">
-                            <img src={item.img} alt={item.title}/>
-                            <span className="card-title">{item.title}</span>
-                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
-                        </div>
+  render() {
+    let itemList = this.props.items.map(item => {
+      return (
+        <div className="card" key={item.id}>
+          <div className="card-image">
+            <img src={item.img} alt={item.title} />
+            <span className="card-title">{item.title}</span>
+            <span
+              to="/"
+              className="btn-floating halfway-fab waves-effect waves-light red"
+              onClick={() => {
+                this.handleClick(item.id);
+              }}
+            >
+              <i className="material-icons">add</i>
+            </span>
+          </div>
 
-                        <div className="card-content">
-                            <p>{item.desc}</p>
-                            <p><b>Price: {item.price}$</b></p>
-                        </div>
-                 </div>
-            )
-        })
+          <div className="card-content">
+            <p>{item.desc}</p>
+            <p>
+              <b>Price: {item.price}$</b>
+            </p>
+          </div>
+        </div>
+      );
+    });
 
-        return(
-            <div className="container">
-                <h3 className="center">Our items</h3>
-                <div className="box">
-                    {itemList}
-                </div>
-            </div>
-        )
-    }
-}
-const mapStateToProps = (state)=>{
-    return {
-      items: state.items
-    }
+    return (
+      <div className="container">
+        <div id="breadcrumb">
+        <Breadcrumb>
+        <BreadcrumbItem><Link to="/">Home</Link></BreadcrumbItem>
+        <BreadcrumbItem active>ALL ILNP NAIL POLISH</BreadcrumbItem>
+      </Breadcrumb>
+        </div>
+        <h3 className="center">ALL ILNP NAIL POLISH</h3>
+        <div className="box">{itemList}</div>
+      </div>
+    );
   }
-const mapDispatchToProps= (dispatch)=>{
-    
-    return{
-        addToCart: (id)=>{dispatch(addToCart(id))}
-    }
 }
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    addToCart: id => {
+      dispatch(addToCart(id));
+    }
+  };
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(Items);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Items);
