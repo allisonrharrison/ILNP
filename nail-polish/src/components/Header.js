fd9f76fcb8640fed9e4b0e0d81b6cb5165abc9de
Allison Harrison <allisonrharrison@gmail.com>
2019-11-11 18:57:20 -0800

    No More Tables

--- nail-polish/src/components/Header.js
+++ nail-polish/src/components/Header.js
@@ -1,48 +1,65 @@
 import React from "react";
-import { Table } from "reactstrap";
 
 const Header = () => {
   return (
     <div className="container col-12 mt-3">
       <div className="row">
-      <div className="col-3">
-          <Table borderless>
-            <tr>
-              <td><i className="fab fa-twitter"></i></td>
-              <td><i className="fab fa-facebook-f"></i></td>
-              <td><i className="fab fa-instagram"></i></td>
-              <td><i className="fab fa-youtube"></i></td>
-              <td><i className="fab fa-pinterest"></i></td>
-              </tr>
-                </Table>
-              <p className="smallText">CONNECT WITH US ON SOCIAL MEDIA</p>
-        </div>
-
-        <div className="col-4 offset-1">
-                <h1>I&nbsp;L&nbsp;N&nbsp;P</h1>
-              <p className="smallText">BOUTIQUE COSMETICS</p>
+        <div className="col-3 offset-1">
+          <div className="row">
+            <i className="fab fa-twitter p-3"></i>
+            <i className="fab fa-facebook-f p-3"></i>
+            <i className="fab fa-instagram p-3"></i>
+            <i className="fab fa-youtube p-3"></i>
+            <i className="fab fa-pinterest p-3"></i>
+          </div>
+          <div className="row py-2">
+            <p className="smallText">CONNECT WITH US ON SOCIAL MEDIA</p>
+          </div>
         </div>
 
         <div className="col-4">
-          <Table borderless>
-            <tr>
-              <td>
-                <i class="fas fa-balance-scale-left"></i>
-              </td>
-              <td>
-                <i className="fas fa-shopping-cart"></i>
-              </td>
-              <td>
-                <i className="fas fa-user-circle"></i>
-              </td>
-            </tr>
-            <tr>
-              <td className="smallText">COMPARE</td>
-              <td className="smallText">MY CART</td>
-              <td className="smallText">ACCOUNT</td>
-            </tr>
-          </Table>
+          <div className="col-4 offset-4">
+          <div className="row">
+          <h1>ILNP</h1>
+          </div>
+          <div className="row">
+          <p className="smallText text-bold">BOUTIQUE COSMETICS</p>
+          </div>
+          </div>
         </div>
+
+        <div className="col-3 offset-1">
+          <div className="row">
+
+          <div className="col-3">
+          <div className="row">
+            <i className="fas fa-balance-scale-left p-3"></i>
+            </div>
+            <div className="row">
+            <p className="smallText py-2">COMPARE</p>
+            </div>
+            </div>
+
+            <div className="col-3">
+          <div className="row">
+          <i className="fas fa-shopping-cart p-3"></i>
+            </div>
+            <div className="row">
+            <p className="smallText py-2">MY CART</p>
+            </div>
+            </div>
+
+            <div className="col-3">
+          <div className="row">
+          <i className="fas fa-user-circle p-3"></i>
+            </div>
+            <div className="row">
+            <p className="smallText py-2">ACCOUNT</p>
+            </div>
+            </div>
+           
+           </div> 
+          </div>
       </div>
     </div>
   );
