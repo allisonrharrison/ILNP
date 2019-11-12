4f49d4234e01bde56d264e303fa7e79b058514d1
Allison Harrison <allisonrharrison@gmail.com>
2019-11-12 14:15:34 -0800

    Cart Functionality

--- nail-polish/src/components/reducers/cartReducer.js
+++ nail-polish/src/components/reducers/cartReducer.js
@@ -1,49 +1,93 @@
-import { ADD_TO_CART} from '../actions/action-types/cart-actions';
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY } from '../actions/action-types/cart-actions'
 
 
 const initState = {
     items: [
-        {id:1,title:'Snowed In Collection', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:62.50},
-        {id:2,title:'Bundled Up', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:10},
-        {id:3,title:'Tinsel', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:10},
-        {id:4,title:'Greatness', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:12.50},
-        {id:5,title:'Christmas Cookie', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:10},
-        {id:6,title:'Cold As Ice', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:10} 
+      {id:1,title:'Snowed In Collection', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:62.50},
+      {id:2,title:'Bundled Up', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:10},
+      {id:3,title:'Tinsel', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:10},
+      {id:4,title:'Greatness', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:12.50},
+      {id:5,title:'Christmas Cookie', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:10},
+      {id:6,title:'Cold As Ice', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:10}
     ],
     addedItems:[],
     total: 0
-  }
 
-    const cartReducer = (state = initState,action)=>{
+}
+const cartReducer= (state = initState,action)=>{
    
-      //INSIDE HOME COMPONENT
-      if(action.type === ADD_TO_CART){
-            let addedItem = state.items.find(item=> item.id === action.id)
-            //check if the action id exists in the addedItems
-           let existed_item= state.addedItems.find(item=> action.id === item.id)
-           if(existed_item)
-           {
-              addedItem.quantity += 1 
-               return{
-                  ...state,
-                   total: state.total + addedItem.price 
-                    }
-          }
-           else{
-              addedItem.quantity = 1;
-              //calculating the total
-              let newTotal = state.total + addedItem.price 
-              
-              return{
-                  ...state,
-                  addedItems: [...state.addedItems, addedItem],
-                  total : newTotal
-              }
-              
-          }
-      }
-      else{
-          return state
-      }
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
     }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+    return state
+}
+
 export default cartReducer;
