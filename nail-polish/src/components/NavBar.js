91079cd59b0a8a8a006d36ed2ab0e988570cddaf
Allison Harrison <allisonrharrison@gmail.com>
2019-11-20 23:28:42 -0800

    Fix Carats

--- nail-polish/src/components/NavBar.js
+++ nail-polish/src/components/NavBar.js
@@ -4,29 +4,39 @@ import { Navbar, Nav, NavItem, Form, Input, Button } from "reactstrap";
 
 const NavBar = () => {
   return (
-      <div className="col-12 mx-auto">
+    <div className="col-12 mx-auto">
       <Navbar expand="lg">
         <Nav className="justify-content border-bottom pb-3 mb-0" navbar>
           <NavItem className="px-4 py-1">
             <Link to="/">HOME</Link>
           </NavItem>
-          <NavItem className="px-4 py-1">
+          <NavItem className="px-3 py-1">
             <Link to="/items">ALL NAIL POLISH</Link>
           </NavItem>
-          <NavItem className="px-4 py-1">
-            <Link to="/">HOLOGRAPHICS <i class="fas fa-sort-down"></i></Link>
+          <NavItem className="px-3 py-1">
+            <Link to="/">
+              HOLOGRAPHICS <i className="fas fa-chevron-down fa-xs fa-fw"></i>
+            </Link>
           </NavItem>
           <NavItem className="px-4 py-1">
-            <Link to="/">ULTRA CHROMES <i class="fas fa-sort-down"></i></Link>
+            <Link to="/">
+              ULTRA CHROMES <i className="fas fa-chevron-down fa-xs fa-fw"></i>
+            </Link>
           </NavItem>
           <NavItem className="px-4 py-1">
-            <Link to="/">ULTRA METALLICS <i class="fas fa-sort-down"></i></Link>
+            <Link to="/">
+              ULTRA METALLICS <i className="fas fa-chevron-down fa-xs fa-fw"></i>
+            </Link>
           </NavItem>
           <NavItem className="px-4 py-1">
-            <Link to="/">JELLIES <i class="fas fa-sort-down"></i></Link>
+            <Link to="/">
+              JELLIES <i className="fas fa-chevron-down fa-xs fa-fw"></i>
+            </Link>
           </NavItem>
           <NavItem className="px-4 py-1">
-            <Link to="/">SHIMMERS <i class="fas fa-sort-down"></i></Link>
+            <Link to="/">
+              SHIMMERS &nbsp;<i className="fas fa-chevron-down fa-xs"></i>
+            </Link>
           </NavItem>
           <NavItem className="px-4 py-1">
             <Link to="/giftcards">GIFT CARDS</Link>
@@ -34,21 +44,21 @@ const NavBar = () => {
           <NavItem className="px-4 py-1">&nbsp;|&nbsp;</NavItem>
           <NavItem className="px-4">
             <Form inline>
-            <Input
-              className="border-0"
-              type="search"
-              placeholder="FIND YOUR POLISH"
-              aria-label="Search"
-              id="search"
-            />
-            <Button className="btn btn-light" type="submit">
-            <i className="fas fa-search"></i>
-            </Button>
-          </Form>
+              <Input
+                className="border-0"
+                type="search"
+                placeholder="FIND YOUR POLISH"
+                aria-label="Search"
+                id="search"
+              />
+              <Button className="btn btn-light" type="submit">
+                <i className="fas fa-search"></i>
+              </Button>
+            </Form>
           </NavItem>
         </Nav>
       </Navbar>
-      </div>
+    </div>
   );
 };
 
