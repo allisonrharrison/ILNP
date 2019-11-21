59c3ed662e54f05a3aaaf216d410210df511b072
Allison Harrison <allisonrharrison@gmail.com>
2019-11-20 21:40:02 -0800

    All Links

--- nail-polish/src/components/Footer.js
+++ nail-polish/src/components/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
             <li className="text-light">
               <strong>CUSTOMER SERVICE</strong>
             </li>
-            <li>Customer Service Home</li>
-            <li>Shipping Information</li>
-            <li>Tracking Orders</li>
-            <li>Lost or Damaged Goods</li>
-            <li>Returns & Exchanges</li>
-            <li>Frequently Asked Questions</li>
-            <li>Contact Us</li>
+            <a className="text-light" href="https://www.ilnp.com/customer-service/"><li>Customer Service Home</li></a>
+            <a className="text-light" href="https://www.ilnp.com/shipping-information-us-canada-international/"><li>Shipping Information</li></a>
+            <a className="text-light" href="https://www.ilnp.com/tracking-orders/"><li>Tracking Orders</li></a>
+            <a className="text-light" href="https://www.ilnp.com/lost-or-damaged-products/"><li>Lost or Damaged Goods</li></a>
+            <a className="text-light" href="https://www.ilnp.com/returns-exchanges/"><li>Returns & Exchanges</li></a>
+            <a className="text-light" href="https://www.ilnp.com/about-our-products/"><li>Frequently Asked Questions</li></a>
+            <a className="text-light" href="https://www.ilnp.com/contact-us/"><li>Contact Us</li></a>
           </ul>
         </div>
         <div className="col-2">
@@ -23,10 +23,10 @@ const Footer = () => {
             <li className="text-light">
               <strong>INFORMATION</strong>
             </li>
-            <li>Cruelty Free / Vegan Policy</li>
-            <li>Free Shipping</li>
-            <li>Nail & Beauty Bloggers</li>
-            <li>Wholesale</li>
+            <a className="text-light" href="https://www.ilnp.com/about-our-products/"><li>Cruelty Free / Vegan Policy</li></a>
+            <a className="text-light" href="https://www.ilnp.com/free-shipping/"><li>Free Shipping</li></a>
+            <a className="text-light" href="https://www.ilnp.com/nail-beauty-bloggers/"><li>Nail & Beauty Bloggers</li></a>
+            <a className="text-light" href="https://www.ilnp.com/wholesale-orders-resellers/"><li>Wholesale</li></a>
           </ul>
         </div>
         <div className="col-2">
@@ -34,22 +34,22 @@ const Footer = () => {
             <li className="text-light">
               <strong>MY ACCOUNT</strong>
             </li>
-            <li>My Account</li>
-            <li>My Orders</li>
-            <li>My Address Book</li>
-            <li>My Reward</li>
-            <li>My Wishlist</li>
-            <li>Check Gift Card Balance</li>
+            <a className="text-light" href="https://www.ilnp.com/my-account/edit-account/"><li>My Account</li></a>
+            <a className="text-light" href="https://www.ilnp.com/my-account/orders/"><li>My Orders</li></a>
+            <a className="text-light" href="https://www.ilnp.com/my-account/edit-address/"><li>My Address Book</li></a>
+            <a className="text-light" href="https://www.ilnp.com/my-account/my-rewards/"><li>My Reward</li></a>
+            <a className="text-light" href="https://www.ilnp.com/my-account/my-wishlist/"><li>My Wishlist</li></a>
+            <a className="text-light" href="https://www.ilnp.com/check-gift-card-html/"><li>Check Gift Card Balance</li></a>
           </ul>
         </div>
         <div className="col-2">
           <ul>
             <li>&nbsp;</li>
-            <li>Privacy Policy</li>
-            <li>Terms & Conditions</li>
-            <li>Disclaimer</li>
-            <li>Sitemap</li>
-            <li>Cookies</li>
+            <a className="text-light" href="https://www.ilnp.com/privacy-policy/"><li>Privacy Policy</li></a>
+            <a className="text-light" href="https://www.ilnp.com/terms/"><li>Terms & Conditions</li></a>
+            <a className="text-light" href="https://www.ilnp.com/disclaimer/"><li>Disclaimer</li></a>
+            <a className="text-light" href="https://www.ilnp.com/sitemap/"> <li>Sitemap</li></a>
+            <a className="text-light" href="https://www.ilnp.com/what-are-cookies/"><li>Cookies</li></a>
           </ul>
         </div>
         <div className="col-2 offset-1 mt-5">
